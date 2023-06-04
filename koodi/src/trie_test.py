2f0055f9ced21ddaac26222eccf8563c2a3d8fdd
Paul <paul.m.sainio@gmail.com>
2023-06-04 22:34:56 +0300

    new tests

--- koodi/src/trie_test.py
+++ koodi/src/trie_test.py
@@ -1,23 +1,49 @@
 import unittest
 from trie import Trie
 
-trie_test = Trie()
-test_data = [1, 2, 1, 3, 1, 4, 1, 5]
-
 
 class TestTrie(unittest.TestCase):
-    def SetUp(self):
-        self.trie = trie_test
-        self.data = test_data
+    def setUp(self):
+        self.t = Trie(1)
+        self.tune_1 = [1, 2, 1, 3, 1, 4]
+        self.tune_2 = [1, 3, 1, 5, 1, 6, 1, 7]
+        self.tune_3 = [1, 3]
 
-    # testataan, että ensimmäisen kerroksen jälkeläiset löytyy
-    def test_insert_root(self):
-        self.trie.insert(self.data)
+    def test_setter_and_getter(self):
+        self.t.insert(self.tune_1)
 
-        self.assertEqual(self.trie.root.children.items, 1)
+        result = self.t.get([1])
+        self.assertEqual(result[0], [2, 3, 4])
 
-    # tätä testiä pitää hioa, testataan toisen polven jälkeläisiä
-    def test_root_children(self):
-        self.trie.insert(self.data)
+    def test_multiple_inserts(self):
+        self.t.insert(self.tune_1)
+        self.t.insert(self.tune_2)
+        self.t.insert(self.tune_3)
 
-        # self.assertEqual(self.trie.root.chidren(1), [2,3,4,5])
+        result = self.t.get([1])
+        self.assertEqual(result[0], [2, 3, 4, 5, 6, 7])
+
+    def test_weights(self):
+        self.t.insert(self.tune_1)
+
+        result = self.t.get([1])
+        self.assertEqual(result[1], [1, 1, 1])
+
+    def test_weight_growth(self):
+        self.t.insert(self.tune_1)
+        self.t.insert(self.tune_2)
+        self.t.insert(self.tune_3)
+
+        result = self.t.get([1])
+        self.assertEqual(result[1], [1, 3, 1, 1, 1, 1])
+
+    # testi toimii, pitää vain formatoida asettelu oikein.
+    def test_path_too_long(self):
+        self.t.get([1, 2, 3])
+
+        self.assertRaises(ValueError(
+            f"Key must be shorter than degree. Current degree is {self.degree}."))
+
+
+if __name__ == '__main__':
+    unittest.main()
