bf272f83c19e70aee0b0b52f3bd6ffa09b6a6ccf
Paul <paul.m.sainio@gmail.com>
2023-07-02 14:43:03 +0300

    bug fixes

--- koodi/src/markov.py
+++ koodi/src/markov.py
@@ -8,15 +8,15 @@ def markov(trie: Trie, n, l, key_empty=False):
     while len(new_tune) != l:
         for j in range(n):
             key = trie.get(new_tune[-n:])
-            if key != ([], []):
-                note = random.choices(
-                    key[0], key[1], k=1)[0]
-                new_tune.append(note)
-            else:
+            if key == ([], []) and len(new_tune) > 0:
                 key_empty = True
                 print(
                     "Algoritmi ajautui umpikujaan, joten sävelmä jäi vajaamittaiseksi. Ei hätää.")
                 break
+            else:
+                note = random.choices(
+                    key[0], key[1], k=1)[0]
+                new_tune.append(note)
             if j == n-1 or len(new_tune) == l:
                 break
         if key_empty:
